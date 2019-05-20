86a4309fa5057e080fac05efa7b2dceba7decab3
Piotr Golonko <piopiotrek008@gmail.com>
2019-05-20 22:40:51 +0200

    test as a unttest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,31 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-assert 'Django' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
+    def test_start_a_list_and_retrive(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish the test!")
+        # User sees a field to enter item
+
+        # types item
+
+        # Item appears 
+
+        # and new empty field. Adds another item
+
+        # Two items
+
+        # Special URL
+
+        # Special URL works
+
+if __name__ == '__main__':
+    unittest.main()
