e106e96f093b81832d71ea52a06ceb24ed036908
Piotr Golonko <piopiotrek008@gmail.com>
2019-05-22 21:32:00 +0200

    New solution to not adding empty field to table

--- lists/views.py
+++ lists/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from lists.models import Item
 
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text','')
-    item.save()
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text',''),})
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+    else:
+        new_item_text = ''
+    return render(request, 'home.html', {'new_item_text': new_item_text})
