cccc9d95bd1758aaa0468c8e9f09f1c76866aad7
Piotr Golonko <piopiotrek008@gmail.com>
2019-05-22 20:51:47 +0200

    check the table for a phrase, helper function refactor

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_a_list_and_retrive(self):
         self.browser.get('http://localhost:8000')
 
@@ -27,10 +32,7 @@ class NewVisitorTest(unittest.TestCase):
         # Item appears 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # and new empty field. Adds another item
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -39,10 +41,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Two items
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly',[row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         
         # Special URL
 
